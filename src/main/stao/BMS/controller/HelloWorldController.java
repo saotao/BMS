ef93178fc448bf34c2a9ce0405f46a2efc494179
shentao6 <shentao6@guazi.com>
2019-01-10 18:23:21 +0800

    基础框架

---
+++ src/main/stao/BMS/controller/HelloWorldController.java
@@ -0,0 +1,18 @@
+package stao.BMS.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@ResponseBody
+public class HelloWorldController {
+
+    @RequestMapping("/hello")
+    public String hello(@RequestParam("str") String hh){
+        return "hello"+hh;
+    }
+}
